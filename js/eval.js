226f29ec934b39f24dbff1dc78d02b14dc5b8ebb
Victor <80902748+leonidasfr65@users.noreply.github.com>
2021-05-03 11:14:19 +0200

    Update eval.js

---
+++ js/eval.js
@@ -0,0 +1,51 @@
+/// Récupération générale
+const frm = document.querySelector("form");
+const submit = frm.lastElementChild;
+
+/// Inputs
+const pseudo = document.getElementById("pseudo");
+const message = document.getElementById("message");
+const color = document.getElementById("color");
+
+/// On test si l'utilisateur reseigne un message afin d'enlever le disabled
+message.addEventListener("keyup", () =>{
+	if (message.value !== "") {
+		submit.disabled = false;
+	}
+});
+
+
+
+// addEventListener sur le submit
+frm.addEventListener("submit", (evt) => {
+    //// On test le champ pseudo, par defaut Chat-Nonyme
+    if (pseudo.value === ""){
+        pseudo.value = "Chat-Nonyme";
+    } else {
+        pseudo.value = pseudo.value;
+    }
+    //// Créer un button delete
+    let del = document.createElement("button");
+    del.innerHTML = "X";
+    del.addEventListener ("click", function() {
+        let element = del.parentNode.parentNode.removeChild(del.parentNode);
+        element.remove();
+    });
+    //// Récupérer champ "ul" et on crée un champ "li"
+    let ul = document.querySelector("ul");
+    let li = document.createElement("li");
+    //// Fixer la couleur choisi par l'internaute 
+    li.style.color = color.value;
+    //// Insérer les champs pseudo, message et button suppression
+    li.innerHTML = "Votre pseudo: " + pseudo.value + "<br>" + "Votre message: "  + message.value;
+    ul.append(li);
+    li.appendChild(del);
+    //// Réinisialiser les champs et focus
+    pseudo.value = ""; // vider le "pseudo"
+    message.value = ""; // vider le "message"
+    submit.disabled = true; // activer disabled dans le button "submit"
+    color.value = "#000000"; // Réinisialiser le champ "color", par defaut black
+    message.focus(); // focus sur le champ message, car pseudo est falcutatif
+    //// stop submit en cas d'erreur
+    evt.preventDefault();
+});
